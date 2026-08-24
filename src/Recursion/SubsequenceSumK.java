629153aa2841abc3c685c3cad51e486dd2eb4e56
DevGuru <guruseelan29aug2005@gmail.com>
2026-08-24 14:00:19 +0530

    Modifying the application to print only one Subsequencne

--- src/Recursion/SubsequenceSumK.java
+++ src/Recursion/SubsequenceSumK.java
@@ -6,29 +6,40 @@ import java.util.List;
 public class SubsequenceSumK {
     public static void main(String[] args) {
         int k = 9;
-        int[] arr = {8, 2, 4, 7, 11, 5};
+        int[] arr = {9, 1, 8, 2, 4, 7, 11, 5};
         List<Integer> list = new ArrayList<>();
         System.out.println("K = "+k);
-        findSubSeqSum(0, list, arr, arr.length, 0, k);
+        boolean subSequenceK  = findSubSeqSum(0, list, arr, arr.length, 0, k);
 
 //        System.out.println("Total subsequence = "+count);
     }
 
-    private static void findSubSeqSum(int i, List<Integer> list, int[] arr,int n, int sum, int k) {
+    private static boolean findSubSeqSum(int i,
+                                      List<Integer> list,
+                                      int[] arr,
+                                      int n,
+                                      int sum,
+                                      int k ) {
         if(i == n){
             if(sum == k){
                 System.out.println(list.toString());
+                return true;
             }
-            return;
+            return false;
         }
 
         list.add(arr[i]);
         sum += arr[i];
-        findSubSeqSum(i+1, list, arr, n, sum, k);
+        if(findSubSeqSum(i+1, list, arr, n, sum, k) == true){
+            return true;
+        }
 
         list.remove(Integer.valueOf(arr[i]));
         sum -= arr[i];
 
-        findSubSeqSum(i+1, list, arr, n, sum, k);
+        if(findSubSeqSum(i+1, list, arr, n, sum, k) == true){
+            return true;
+        }
+        return false;
     }
 }
