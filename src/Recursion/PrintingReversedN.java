24ffdeec2f4b4ac7fae2c1851c1c22b77f53f7b5
DevGuru <guruseelan29aug2005@gmail.com>
2026-08-24 09:49:51 +0530

    printing N - 1 with Recursion

---
+++ src/Recursion/PrintingReversedN.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PrintingReversedN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+        print(n);
+    }
+
+    private static void print(int n) {
+        if(n < 1){
+            return;
+        }
+        System.out.println(n);
+        print(n-1);
+    }
+}
